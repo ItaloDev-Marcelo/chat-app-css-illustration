5815768c52a0201c90ddc92d87e747def4182618
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-07-03 12:08:21 -0300

    feat: creating context.tsx

---
+++ src/components/Content.tsx
@@ -0,0 +1,13 @@
+
+const Content = () => {
+  return (
+    <section className="text-center xl:text-justify">
+        <h1 className="text-purple-950 text-[2em] xl:text-[2.5em]  my-7 xl:my-3 font-bold">Simple booking</h1>
+        <p className="text-gray-500 px-4 md:text-[1.2em] xl:text-[.9em] xl:px-0 xl:w-110">Stay in touch with our dog walkers through the chat <br className=' hidden xl:flex' /> interface. This makes it easy to 
+  discuss arrangements and  <br className=' hidden xl:flex' /> make bookings. Once the walk has been completed you can  <br className=' hidden xl:flex' /> rate 
+  your walker and book again all through the chat.</p>
+    </section>
+  )
+}
+
+export default Content
