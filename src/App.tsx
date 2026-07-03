7c69d2f316cd948c1eb5303666d5b76941702d4c
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-07-03 13:47:47 -0300

    style: add more style

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <MainLayout>
-       <div className='relative flex flex-col py-10 xl:py-0 xl:flex-row items-center xl:justify-between xl:w-[60vw]  z-1000 left-[25%] xl2:left-[45%] md:scale-109 md:top-10 md:left-[40%] xl:scale-100 xl:top-5 xl2:top-25 top-0 xl2:scale-120'>
+       <div className='relative flex flex-col py-10 xl:py-0 xl:flex-row items-center justify-center xl:justify-between xl:w-[60vw] z-1000 left-[25%] xl2:left-[45%] md:scale-109 md:top-10 md:left-[40%] xl:scale-100 xl:top-5 xl2:top-25 top-0 xl2:scale-120'>
            <Layout/>
            <Content/>
        </div>
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,9 +1,9 @@
 
 const Content = () => {
   return (
-    <section className="text-center xl:text-justify">
+    <section className="text-center xl:text-justify  ml-2 xl:ml-0">
         <h1 className="text-purple-950 text-[2em] xl:text-[2.5em]  my-7 xl:my-3 font-bold">Simple booking</h1>
-        <p className="text-gray-500 px-4 md:text-[1.2em] xl:text-[.9em] xl:px-0 xl:w-110">Stay in touch with our dog walkers through the chat <br className=' hidden xl:flex' /> interface. This makes it easy to 
+        <p className="text-gray-500 w-82  px-2 md:text-[1.2em] xl:text-[.9em] xl:px-0 xl:w-110">Stay in touch with our dog walkers through the chat <br className=' hidden xl:flex' /> interface. This makes it easy to 
   discuss arrangements and  <br className=' hidden xl:flex' /> make bookings. Once the walk has been completed you can  <br className=' hidden xl:flex' /> rate 
   your walker and book again all through the chat.</p>
     </section>
